dc775d84d665375a71df82536107ee13322fef33
Shaclow <1048380960@qq.com>
2019-03-31 20:49:58 +0800

    first commit

---
+++ src/main/java/service/Greeting.java
@@ -0,0 +1,20 @@
+package service;
+/*
+ * 资源表示类
+ */
+public class Greeting {
+	private final long id;
+	private final String content;
+	public Greeting(long id,String content) {
+		this.id=id;
+		this.content=content;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+}
